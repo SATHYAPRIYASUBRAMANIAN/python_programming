5f90a430b12021c1253295fde93fdbe09bc470c4
SATHYAPRIYASUBRAMANIAN <36298843+SATHYAPRIYASUBRAMANIAN@users.noreply.github.com>
2018-02-10 17:32:55 +0530

    Create vowels.py

---
+++ beginner/vowels.py
@@ -0,0 +1,5 @@
+v=input("enter the value")
+if(v=='a'or v=='e'or v=='i' or v=='o'or v=='u' or v=='A' or v=='E'or v=='I'or v=='O'or v=='U'):
+    print("It is an vowel")
+else:
+    print("It is not an vowel")
