4e0f17b08cc356edf07e99fe651da810da7512e7
SATHYAPRIYASUBRAMANIAN <36298843+SATHYAPRIYASUBRAMANIAN@users.noreply.github.com>
2018-03-16 17:58:26 +0530

    Rename factorial.py to factorial1.py

---
+++ beginner/factorial1.py
@@ -0,0 +1,11 @@
+
+num=int(input("enter the number:"))
+fact=1
+if num<0:
+    print("enter a positive  number")
+elif num==0:
+    print("the factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        fact=fact*i
+    print("the factorial of",num,"is",fact)
